89a8823c9fba91bf22ef3c64d5d8917c023bab3a
GodoyAlan <108708002+GodoyAlan@users.noreply.github.com>
2023-08-27 23:17:31 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// nombre y apellido
+var nombre = "Marcos";
+var apellido = "Pérez";
+
+console.log(nombre + " " + apellido)
+
+// Sumas
+var num1= 15;
+var num2= 25;
+var suma = num1+ num2;
+
+console.log(suma);
+
+// Suma con otro numero
+var num3= 35;
+var newsuma= suma + num3;
+
+console.log(newsuma);
